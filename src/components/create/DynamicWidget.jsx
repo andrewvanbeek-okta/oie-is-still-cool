f6b4a19b98cfa7c2fcfef9d86c3f1f1c4c3fa7d8
andrewvanbeektech <42575420+andrewvanbeektech@users.noreply.github.com>
2021-06-25 14:27:35 -0500

    widgets are now created in the ui

--- src/components/create/DynamicWidget.jsx
+++ src/components/create/DynamicWidget.jsx
@@ -3,102 +3,12 @@ import React, { useEffect, useRef, Component } from 'react';
 import { useOktaAuth } from '@okta/okta-react';
 import * as OktaSignIn from '@okta/okta-signin-widget';
 import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css';
+import gql from 'graphql-tag'
+import { Accordion, Button, Icon } from 'semantic-ui-react';
+import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync'
 
 import config from '../../config';
 
-// const DynamicWidget = ({ customTitle }) => {
-//   const { oktaAuth } = useOktaAuth();
-//   const widgetRef = useRef();
-//   console.log(customTitle)
-//   console.log("YOOO")
-//   console.log(this)
-
-//   function Welcome(props) {
-//     console.log("this called")
-//     console.log(props)
-//   }
-
-//   useEffect(() => {
-//     if (!widgetRef.current) {
-//       return false;
-//     }
-//     console.log(customTitle)
-//     Welcome()
-//     const { issuer, clientId, redirectUri, scopes } = config.oidc;
-//     const widget = new OktaSignIn({
-//       /**
-//        * Note: when using the Sign-In Widget for an OIDC flow, it still
-//        * needs to be configured with the base URL for your Okta Org. Here
-//        * we derive it from the given issuer for convenience.
-//        */
-//       baseUrl: issuer.split('/oauth2')[0],
-//       clientId,
-//       redirectUri,
-//       logo: '/react.svg',
-//       features: {
-//         registration: true, // Enable self-service registration flow
-//       },
-//       idps: [
-//         { type: 'GOOGLE', id: '0oas1xf52O9XhIAYb5d6' },
-//         // { type: 'OKTA', id: '0oas1xf52O9XhIAYb5d6', text: 'Sign in with Okta', logo: '/react.svg' },
-//         // { type: 'FACEBOOK', id: '0oas1xf52O9XhIAYb5d6' },
-//         // { type: 'APPLE', id: '0oas1xf52O9XhIAYb5d6' },
-//         // { type: 'LINKEDIN', id: '0oas1xf52O9XhIAYb5d6' },
-//         // { type: 'MICROSOFT', id: '0oas1xf52O9XhIAYb5d6' },
-//       ],
-//       i18n: {
-//         en: {
-//           'primaryauth.title': Object.values({ customTitle }),
-//         },
-//       },
-//       authParams: {
-//         // To avoid redirect do not set "pkce" or "display" here. OKTA-335945
-//         issuer,
-//         scopes,
-//       },
-//       registration: {
-//       },
-//       /* registration: {
-//         parseSchema: function(schema, onSuccess, onFailure) {
-//           // handle parseSchema callback
-//           onSuccess(schema);
-//       },
-//          preSubmit: function (postData, onSuccess, onFailure) {
-//            // handle preSubmit callback
-//            onSuccess(postData);
-//       },
-//          postSubmit: function (response, onSuccess, onFailure) {
-//           // handle postsubmit callback
-//           onSuccess(response);
-//       }
-//     },
-//     features: {
-//       // Used to enable registration feature on the widget.
-//       // https://github.com/okta/okta-signin-widget#feature-flags
-//        registration: true // REQUIRED
-//     }, */
-//     });
-
-//     widget.showSignInToGetTokens({
-//       el: widgetRef.current,
-//       scopes,
-//     }).then((tokens) => {
-//       // Add tokens to storage
-//       oktaAuth.handleLoginRedirect(tokens);
-//     }).catch((err) => {
-//       throw err;
-//     });
-
-//     return () => widget.remove();
-//   }, [oktaAuth, customTitle]);
-
-//   return (
-//     <div>
-//       <div className="dynamicWidgetContainer" ref={widgetRef} />
-//     </div>
-//   );
-// };
-// export default DynamicWidget;
 
 export default class DynamicWidget extends Component {
   constructor(props) {
@@ -110,6 +20,7 @@ export default class DynamicWidget extends Component {
     const { issuer, clientId, redirectUri, scopes } = config.oidc;
     this.config = {
       baseUrl: issuer.split('/oauth2')[0],
+      title: this.customTitle,
       clientId,
       redirectUri,
       logo: '/react.svg',
@@ -130,6 +41,27 @@ export default class DynamicWidget extends Component {
       registration: {},
     };
   }
+
+  mutate = gql(`
+  mutation createWidget($createwidgetinput: CreateWidgetInput!) {
+    createWidget(input: $createwidgetinput) {
+      name
+      id
+      title
+      owner
+      client_id
+      base_url
+      js_config
+      css_config
+      issuer
+      img
+      name
+      description
+      link
+      logo_url
+    }
+  }
+`)
 
   componentWillReceiveProps(nextProps) {
     console.log(this.wrapper.current.children)
@@ -183,8 +115,7 @@ export default class DynamicWidget extends Component {
     //   }
     // }
     console.log('this is our socials:', socialproviders);
-
-    this.widget = new OktaSignIn({
+    this.config = {
       baseUrl: issuer.split('/oauth2')[0],
       clientId,
       redirectUri,
@@ -204,7 +135,9 @@ export default class DynamicWidget extends Component {
         scopes,
       },
       registration: {},
-    });
+    }
+
+    this.widget = new OktaSignIn(this.config);
     this.widget
       .showSignInToGetTokens({
         el: this.wrapper.current,
@@ -219,6 +152,46 @@ export default class DynamicWidget extends Component {
       });
 
     return () => widget.remove();
+  }
+
+  vari = {
+    "name": "jk",
+  }
+
+  // title: String
+	// owner: String
+	// client_id: String
+	// base_url: String
+	// js_config: String
+	// css_config: String
+	// issuer: String
+	// img: String
+	// name: String
+	// description: String
+	// link: String
+	// logo_url: String
+  submitWidget = async() => {
+    var token = JSON.parse(localStorage.getItem("okta-token-storage"))
+    console.log(token.accessToken.value)
+    this.vari.name = this.config.title
+    this.vari.title = this.config.title
+    this.vari.logo_url = this.config.logo
+    this.vari.img = this.config.img
+    var graphqlClient = new AWSAppSyncClient({
+      url: "https://my3tbw2e4ngbjaq3dxnbra3fhe.appsync-api.us-west-2.amazonaws.com/graphql",
+      region: "us-west-2",
+      disableOffline: true,
+      auth: {
+        type: AUTH_TYPE.OPENID_CONNECT,
+        jwtToken: token.accessToken.value,
+      },
+    });
+    try {
+      var response = await graphqlClient.mutate({ mutation: this.mutate, variables:{createwidgetinput: this.vari} })
+    console.log(response.data)
+    } catch(e) {
+      console.log(e)
+    }
   }
 
   componentDidMount() {
@@ -246,6 +219,6 @@ export default class DynamicWidget extends Component {
   }
 
   render() {
-    return <div ref={this.wrapper} />;
+    return <div><Button onClick={this.submitWidget}floated='right'>Right Floated</Button><div ref={this.wrapper} /></div>;
   }
 }
